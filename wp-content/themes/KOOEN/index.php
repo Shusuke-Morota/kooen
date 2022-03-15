a20c942b5ca15f40ea52e29b862f7a4cfdf98a90
Shusuke-Morota <shusuke.7.tommy@gmail.com>
2022-03-15 23:45:40 +0900

    [add:] index.phpにおいて、first viewの画像群を追加。

--- wp-content/themes/KOOEN/index.php
+++ wp-content/themes/KOOEN/index.php
@@ -1,1 +1,23 @@
 <?php get_header(); ?>
+<main>
+  <div class="topPage">
+    <section class="firstView">
+      <div class="inner">
+        <h1 class="logo">
+          <video src="assets/img/home/videos/fv_animation.mp4" autoplay muted></video>
+        </h1>
+        <ul class="arrows">
+          <li class="arrow">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/contact_arrow.svg" alt="arrow">
+          </li>
+          <li class="arrow">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/contact_arrow.svg" alt="arrow">
+          </li>
+          <li class="arrow">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/contact_arrow.svg" alt="arrow">
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</main>
