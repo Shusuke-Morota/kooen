9709fc403dc38e937052732f150ec47098cad760
Shusuke-Morota <shusuke.7.tommy@gmail.com>
2022-03-17 23:19:36 +0900

    [add:] index.phpにおいて、三段矢印のanimationを追加。

--- wp-content/themes/KOOEN/index.php
+++ wp-content/themes/KOOEN/index.php
@@ -4,23 +4,24 @@
     <section class="firstView">
       <div class="inner">
         <h1 class="logo">
-          <!-- <video src="assets/img/home/videos/fv_animation.mp4" autoplay muted></video> -->
-          <p class="box">mp4</p>
+          <video class="logoVideo" src="<?php echo get_template_directory_uri(); ?>/assets/img/home/videos/fv_animation.mp4" autoplay muted playsinline></video>
         </h1>
         <ul class="arrows">
           <li class="arrow">
             <svg width="54" height="22" viewBox="0 0 54 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path class="modifiedArrow" d="M1.00002 2L26.9808 20L52.9615 2" stroke="#01D480" stroke-opacity="0.2" stroke-width="3"></path>
+              <path class="topArrowAnimation" d="M1.00002 2L26.9808 20L52.9615 2" stroke="#01D480" stroke-opacity="0.2" stroke-width="3">
+              </path>
             </svg>
           </li>
           <li class="arrow">
             <svg width="54" height="22" viewBox="0 0 54 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path class="modifiedArrow" d="M1.00002 2L26.9808 20L52.9615 2" stroke="#01D480" stroke-opacity="0.2" stroke-width="3"></path>
+            <path class="middleArrowAnimation" d="M1.00002 2L26.9808 20L52.9615 2" stroke="#01C18D" stroke-opacity="0.5" stroke-width="3"></path>
             </svg>
           </li>
           <li class="arrow">
             <svg width="54" height="22" viewBox="0 0 54 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path class="modifiedArrow" d="M1.00002 2L26.9808 20L52.9615 2" stroke="#01D480" stroke-opacity="0.2" stroke-width="3"></path>
+              <path class="bottomArrowAnimation" d="M1.00002 2L26.9808 20L52.9615 2" stroke="#01AB9D" stroke-width="3"></path>
+              </path>
             </svg>
           </li>
         </ul>
